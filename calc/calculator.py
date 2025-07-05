9c4cd459f87aa7df5c12420dea30afdde4ebd45e
Tithaal Sheth <tithaalsheth1@gmail.com>
2025-07-05 17:16:11 +0530

    Step 5:Passed test for Supporting newline as a delimiter

--- calc/calculator.py
+++ calc/calculator.py
@@ -2,6 +2,10 @@ def add(numbers):
     if numbers == "":
         return 0
     
+    for newL in numbers:
+        if newL == "\n":
+            numbers = numbers.replace("\n", "")
+    
     num= numbers.split(",")
     total_sum=0
     for i in num:
